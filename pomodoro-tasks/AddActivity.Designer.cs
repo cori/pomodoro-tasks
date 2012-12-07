af96b89548bb9913455ca76505089a78c7e345b4
cori schlegel <cori.schlegel@gmail.com>
2012-12-07 09:36:00 -0600

    query database and bind list boxes, refresh on load and on add

--- pomodoro-tasks/AddActivity.Designer.cs
+++ pomodoro-tasks/AddActivity.Designer.cs
@@ -40,32 +40,35 @@
             this.list_activities = new System.Windows.Forms.ListBox();
             this.btn_move_activity_to_tasks = new System.Windows.Forms.Button();
             this.btn_move_task_to_activities = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txt_activity_description
             // 
-            this.txt_activity_description.Location = new System.Drawing.Point(12, 37);
+            this.txt_activity_description.CausesValidation = false;
+            this.txt_activity_description.Location = new System.Drawing.Point(6, 41);
             this.txt_activity_description.Name = "txt_activity_description";
             this.txt_activity_description.Size = new System.Drawing.Size(322, 20);
             this.txt_activity_description.TabIndex = 0;
             this.txt_activity_description.TextChanged += new System.EventHandler(this.txtbox_TextChanged);
-            this.txt_activity_description.Validating += new System.ComponentModel.CancelEventHandler(this.txt_activity_description_Validating);
             // 
             // btn_add_activity
             // 
-            this.btn_add_activity.Location = new System.Drawing.Point(452, 35);
+            this.btn_add_activity.Location = new System.Drawing.Point(446, 39);
             this.btn_add_activity.Name = "btn_add_activity";
             this.btn_add_activity.Size = new System.Drawing.Size(75, 23);
             this.btn_add_activity.TabIndex = 2;
             this.btn_add_activity.Text = "Add Activity";
             this.btn_add_activity.UseVisualStyleBackColor = true;
             this.btn_add_activity.Click += new System.EventHandler(this.btn_add_activity_Click);
+            this.btn_add_activity.Validating += new System.ComponentModel.CancelEventHandler(this.add_activity_Validating);
             // 
             // lbl_actity_description
             // 
             this.lbl_actity_description.AutoSize = true;
-            this.lbl_actity_description.Location = new System.Drawing.Point(12, 18);
+            this.lbl_actity_description.Location = new System.Drawing.Point(6, 22);
             this.lbl_actity_description.Name = "lbl_actity_description";
             this.lbl_actity_description.Size = new System.Drawing.Size(97, 13);
             this.lbl_actity_description.TabIndex = 3;
@@ -74,7 +77,7 @@
             // lbl_actity_estimate
             // 
             this.lbl_actity_estimate.AutoSize = true;
-            this.lbl_actity_estimate.Location = new System.Drawing.Point(346, 17);
+            this.lbl_actity_estimate.Location = new System.Drawing.Point(340, 21);
             this.lbl_actity_estimate.Name = "lbl_actity_estimate";
             this.lbl_actity_estimate.Size = new System.Drawing.Size(81, 13);
             this.lbl_actity_estimate.TabIndex = 4;
@@ -82,7 +85,8 @@
             // 
             // txt_activity_estimate
             // 
-            this.txt_activity_estimate.Location = new System.Drawing.Point(349, 36);
+            this.txt_activity_estimate.CausesValidation = false;
+            this.txt_activity_estimate.Location = new System.Drawing.Point(343, 40);
             this.txt_activity_estimate.Mask = "0";
             this.txt_activity_estimate.Name = "txt_activity_estimate";
             this.txt_activity_estimate.PromptChar = '?';
@@ -90,7 +94,6 @@
             this.txt_activity_estimate.TabIndex = 2;
             this.txt_activity_estimate.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txt_activity_estimate_MaskInputRejected);
             this.txt_activity_estimate.TextChanged += new System.EventHandler(this.txtbox_TextChanged);
-            this.txt_activity_estimate.Validating += new System.ComponentModel.CancelEventHandler(this.add_activity_form_Validating);
             // 
             // errorProvider1
             // 
@@ -98,7 +101,9 @@
             // 
             // btn_exit
             // 
-            this.btn_exit.Location = new System.Drawing.Point(453, 447);
+            this.btn_exit.CausesValidation = false;
+            this.btn_exit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_exit.Location = new System.Drawing.Point(453, 480);
             this.btn_exit.Name = "btn_exit";
             this.btn_exit.Size = new System.Drawing.Size(75, 23);
             this.btn_exit.TabIndex = 5;
@@ -107,7 +112,8 @@
             // 
             // btn_view_task_list
             // 
-            this.btn_view_task_list.Location = new System.Drawing.Point(372, 447);
+            this.btn_view_task_list.CausesValidation = false;
+            this.btn_view_task_list.Location = new System.Drawing.Point(372, 480);
             this.btn_view_task_list.Name = "btn_view_task_list";
             this.btn_view_task_list.Size = new System.Drawing.Size(75, 23);
             this.btn_view_task_list.TabIndex = 6;
@@ -117,7 +123,7 @@
             // list_tasks
             // 
             this.list_tasks.FormattingEnabled = true;
-            this.list_tasks.Location = new System.Drawing.Point(297, 64);
+            this.list_tasks.Location = new System.Drawing.Point(297, 97);
             this.list_tasks.Name = "list_tasks";
             this.list_tasks.Size = new System.Drawing.Size(231, 368);
             this.list_tasks.TabIndex = 7;
@@ -125,14 +131,15 @@
             // list_activities
             // 
             this.list_activities.FormattingEnabled = true;
-            this.list_activities.Location = new System.Drawing.Point(12, 63);
+            this.list_activities.Location = new System.Drawing.Point(12, 96);
             this.list_activities.Name = "list_activities";
             this.list_activities.Size = new System.Drawing.Size(231, 368);
             this.list_activities.TabIndex = 8;
             // 
             // btn_move_activity_to_tasks
             // 
-            this.btn_move_activity_to_tasks.Location = new System.Drawing.Point(250, 183);
+            this.btn_move_activity_to_tasks.CausesValidation = false;
+            this.btn_move_activity_to_tasks.Location = new System.Drawing.Point(250, 234);
             this.btn_move_activity_to_tasks.Name = "btn_move_activity_to_tasks";
             this.btn_move_activity_to_tasks.Size = new System.Drawing.Size(41, 23);
             this.btn_move_activity_to_tasks.TabIndex = 9;
@@ -141,36 +148,49 @@
             // 
             // btn_move_task_to_activities
             // 
-            this.btn_move_task_to_activities.Location = new System.Drawing.Point(250, 230);
+            this.btn_move_task_to_activities.CausesValidation = false;
+            this.btn_move_task_to_activities.Location = new System.Drawing.Point(250, 281);
             this.btn_move_task_to_activities.Name = "btn_move_task_to_activities";
             this.btn_move_task_to_activities.Size = new System.Drawing.Size(41, 23);
             this.btn_move_task_to_activities.TabIndex = 10;
             this.btn_move_task_to_activities.Text = "<<";
             this.btn_move_task_to_activities.UseVisualStyleBackColor = true;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_actity_description);
+            this.groupBox1.Controls.Add(this.txt_activity_description);
+            this.groupBox1.Controls.Add(this.btn_add_activity);
+            this.groupBox1.Controls.Add(this.lbl_actity_estimate);
+            this.groupBox1.Controls.Add(this.txt_activity_estimate);
+            this.groupBox1.Location = new System.Drawing.Point(6, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(530, 72);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add an activity";
+            // 
             // AddActivity
             // 
             this.AcceptButton = this.btn_add_activity;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 482);
+            this.CancelButton = this.btn_exit;
+            this.ClientSize = new System.Drawing.Size(540, 511);
             this.Controls.Add(this.btn_move_task_to_activities);
             this.Controls.Add(this.btn_move_activity_to_tasks);
             this.Controls.Add(this.list_activities);
             this.Controls.Add(this.list_tasks);
             this.Controls.Add(this.btn_view_task_list);
             this.Controls.Add(this.btn_exit);
-            this.Controls.Add(this.txt_activity_estimate);
-            this.Controls.Add(this.lbl_actity_estimate);
-            this.Controls.Add(this.lbl_actity_description);
-            this.Controls.Add(this.btn_add_activity);
-            this.Controls.Add(this.txt_activity_description);
+            this.Controls.Add(this.groupBox1);
             this.Name = "AddActivity";
-            this.Text = "Add Activity";
+            this.Text = "Activities";
             this.Load += new System.EventHandler(this.AddActivity_Load);
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -188,5 +208,6 @@
         private System.Windows.Forms.ListBox list_tasks;
         private System.Windows.Forms.Button btn_view_task_list;
         private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- pomodoro-tasks/AddActivity.cs
+++ pomodoro-tasks/AddActivity.cs
@@ -15,17 +15,35 @@ namespace pomodoro_tasks
 
         private void AddActivity_Load(object sender, EventArgs e)
         {
-
             ReloadListBoxes();
         }
 
         private void ReloadListBoxes()
         {
+            Program.connection.Open();
+            SQLiteCommand getActivities = new SQLiteCommand("select activity_desc, estimated from activities where is_active_task = 0", Program.connection);
+            SQLiteCommand getTasks = new SQLiteCommand("select activity_desc, estimated from activities where is_active_task = 1", Program.connection);
+            //var activitiesReader = getActivities.ExecuteReader();
+            //var tasksReader = getTasks.ExecuteReader();
+            //var activityVals = activitiesReader.GetValues();
+            //var taskVals = tasksReader.GetValues();
 
-            SQLiteCommand getActivities = "select activity_desc, estimated from activities where is_active_task = 0";
-            SQLiteCommand getTasks = "select activity_desc, estimated from activities where is_active_task = 1";
 
-            throw new NotImplementedException();
+            SQLiteDataAdapter activitiesAdapter = new SQLiteDataAdapter(getActivities);
+            SQLiteDataAdapter tasksAdapter = new SQLiteDataAdapter(getTasks);
+            DataSet activitiesDS = new DataSet();
+            DataSet tasksDS = new DataSet();
+            activitiesAdapter.Fill(activitiesDS);
+            tasksAdapter.Fill(tasksDS);
+            list_activities.DataSource = activitiesDS.Tables[0];
+            list_tasks.DataSource = tasksDS.Tables[0];
+
+            list_activities.DisplayMember = "activity_desc";
+            list_tasks.DisplayMember = "activity_desc";
+            list_activities.Refresh();
+            list_tasks.Refresh();
+            Program.connection.Close();
+
         }
 
         public AddActivity()
@@ -35,24 +53,27 @@ namespace pomodoro_tasks
 
         private void btn_add_activity_Click(object sender, EventArgs e)
         {
-            this.ValidateChildren();
-            var activity = new Activity {
-                Description = txt_activity_description.Text,
-                EstimatedPomodoros = int.Parse(txt_activity_estimate.Text),
-                ActualPomodoros = 0,
-                Interruptions = 0,
-                IsActiveTask = false,
-                IsCompleted = false
-            };
+            if (this.ValidateChildren()) {
+                var activity = new Activity {
+                    Description = txt_activity_description.Text,
+                    EstimatedPomodoros = int.Parse(txt_activity_estimate.Text),
+                    ActualPomodoros = 0,
+                    Interruptions = 0,
+                    IsActiveTask = false,
+                    IsCompleted = false
+                };
 
-            SQLiteCommand cmd = new SQLiteCommand(activity.MakeInsertStatement(), Program.connection);
-            Program.connection.Open();
-            cmd.ExecuteNonQuery();
-            Program.connection.Close();
+                SQLiteCommand cmd = new SQLiteCommand(activity.MakeInsertStatement(), Program.connection);
+                Program.connection.Open();
+                cmd.ExecuteNonQuery();
+                Program.connection.Close();
 
-            txt_activity_description.Clear();
-            txt_activity_description.Focus();
-            txt_activity_estimate.Clear();
+                txt_activity_description.Clear();
+                txt_activity_description.Focus();
+                txt_activity_estimate.Clear();
+
+                ReloadListBoxes();
+            }
 
         }
 
@@ -61,29 +82,33 @@ namespace pomodoro_tasks
             this.errorProvider1.SetError(this.txt_activity_estimate,"Only numeric values are valid");
         }
 
-        private void add_activity_form_Validating(object sender, CancelEventArgs e)
-        {
-            var cancel = false;
-            int val;
-            if(!int.TryParse(txt_activity_estimate.Text, out val)){
-                cancel = true;
-                this.errorProvider1.SetError(this.txt_activity_estimate, "Only numeric values are valid");
-            }
-            e.Cancel = cancel;
-        }
+        //private void add_activity_form_Validating(object sender, CancelEventArgs e)
+        //{
+        //    var cancel = false;
+        //    int val;
+        //    if(!int.TryParse(txt_activity_estimate.Text, out val)){
+        //        cancel = true;
+        //        this.errorProvider1.SetError(this.txt_activity_estimate, "Only numeric values are valid");
+        //    }
+        //    e.Cancel = cancel;
+        //}
 
         private void txtbox_TextChanged(object sender, EventArgs e)
         {
            this.errorProvider1.Clear();
         }
 
-        private void txt_activity_description_Validating(object sender, CancelEventArgs e)
+        private void add_activity_Validating(object sender, CancelEventArgs e)
         {
             var cancel = false;
             int val;
             if (string.IsNullOrWhiteSpace(this.txt_activity_description.Text)) {
                 cancel = true;
                 this.errorProvider1.SetError(this.txt_activity_description, "Activity name must be provided");
+            }
+            if (!int.TryParse(txt_activity_estimate.Text, out val)) {
+                cancel = true;
+                this.errorProvider1.SetError(this.txt_activity_estimate, "Only numeric values are valid");
             }
             e.Cancel = cancel;
         }
